37cb6fdc0479c960f21517086d2eeae339836aa5
thinhddo <thinh.dd@cyberlogitec.com>
2023-09-05 21:48:06 +0700

    #2 feat: add font, css, text and image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   // darkMode: "class",
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightgray: "hsl(212, 45%, 89%)",
+        white: "hsl(0, 0%, 100%)",
+        grayishblue: "hsl(220, 15%, 55%)",
+        darkblue: "hsl(218, 44%, 22%)",
+      },
+    },
+    fontWeight: {
+      bold: 400,
+      extrabold: 700
+    },
   },
   plugins: [],
 }
